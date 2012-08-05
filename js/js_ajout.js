1f9ef7c34b1fdaaaff812ffd1f549aac54a1cbb9
Bastien Louërat <b.louerat@gmail.com>
2012-08-05 04:55:07 +0200

    big overnight commit.
    Sorry for that.

--- js/js_ajout.js
+++ js/js_ajout.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-	$('.icones_find_me').click(function(){
-		var id_encours = $(this).attr('id');
-		var alt_encours = $(this).attr('alt');
-		$(this).hide();
-		$('#' + id_encours + '_ok').attr('src', 'img/icones/' + alt_encours + '_ok.png');
-		var alt_encours = $('#' + id_encours + '_ok').attr('alt');
-		alert('GG vous avez trouv\351 ' + alt_encours + '!');
+	$('.hiddenObject').click(function(){
+		$(this).fadeOut(500, function(){
+		  $(this).remove();
+		  $('#desc_' + $(this).attr('id')).remove();
+		  if(isWin()) onWin();
+	  });
 	});
+	
+	$('.carousel')
+	.carousel( { interval: false } )
+	.on('slide', slideScreenUpdate)
+	.on('slid', slidScreenUpdate);
 });
+
+var slideScreenUpdate = function(e) { planIcons(slideEventActiveId(e)).fadeOut(100); };
+var slidScreenUpdate = function(e) { planIcons(slideEventActiveId(e)).show(); };
+function slideEventActiveId(e) {return e.currentTarget.getElementsByClassName('item active')[0].id; };
+function planIcons(planId) { return $('.hiddenObject.' + planId); };
+function isWin() {return $('.hiddenObject').length == 0};
+function onWin() {alert('win!');}
+
